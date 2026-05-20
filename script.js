741ea49345249b74bd7d86500de167f3d91f1b76
KRXZY <slurptheramen21@gmail.com>
2026-05-20 17:00:55 +0800

    Update GIF URLs in gifStages array

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const gifStages = [
-    "https://media.tenor.com/EBV7OT7ACfwAAAAj/u-u-qua-qua-u-quaa.gif",    // 0 normal
-    "https://media1.tenor.com/m/uDugCXK4vI4AAAAd/chiikawa-hachiware.gif",  // 1 confused
-    "https://media.tenor.com/f_rkpJbH1s8AAAAj/somsom1012.gif",             // 2 pleading
+    "https://tenor.com/view/um-gif-1318300803934784345",    // 0 normal
+    "https://tenor.com/view/sideye-gif-12305010564308244332",  // 1 confused
+    "https://tenor.com/view/stickericin-gif-18191458991849826004",             // 2 pleading
     "https://media.tenor.com/OGY9zdREsVAAAAAj/somsom1012.gif",             // 3 sad
     "https://media1.tenor.com/m/WGfra-Y_Ke0AAAAd/chiikawa-sad.gif",       // 4 sadder
     "https://media.tenor.com/CivArbX7NzQAAAAj/somsom1012.gif",             // 5 devastated
